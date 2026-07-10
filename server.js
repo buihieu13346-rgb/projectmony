c41b923b05a259c56d0fb0a93142a65c7d71a266
buihieu13346-rgb <buihieu13346@gmail.com>
2026-07-11 00:28:21 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,147 @@
+const express=require("express");
+const cors=require("cors");
+const fs=require("fs");
+
+
+const app=express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+function readUsers(){
+
+return JSON.parse(
+fs.readFileSync("users.json")
+);
+
+}
+
+
+
+function saveUsers(data){
+
+fs.writeFileSync(
+"users.json",
+JSON.stringify(data,null,2)
+);
+
+}
+
+
+
+
+// đăng nhập
+
+app.post("/login",(req,res)=>{
+
+
+let {
+username,
+password,
+device
+}=req.body;
+
+
+
+let users=readUsers();
+
+
+
+let user=
+users.find(
+u=>u.username==username
+);
+
+
+
+// tạo tài khoản mới
+
+if(!user){
+
+
+users.push({
+
+username,
+password,
+device
+
+});
+
+
+saveUsers(users);
+
+
+return res.json({
+
+ok:true
+
+});
+
+
+}
+
+
+
+
+// sai mật khẩu
+
+if(user.password!=password){
+
+return res.json({
+
+ok:false,
+msg:"Sai mật khẩu"
+
+});
+
+}
+
+
+
+
+// khác thiết bị
+
+if(user.device!=device){
+
+return res.json({
+
+ok:false,
+
+msg:"Tài khoản đang dùng trên thiết bị khác"
+
+});
+
+}
+
+
+
+res.json({
+
+ok:true
+
+});
+
+
+
+});
+
+
+
+
+
+app.use(
+express.static(".")
+);
+
+
+
+app.listen(3000,()=>{
+
+console.log(
+"Server chạy tại http://localhost:3000"
+);
+
+});
